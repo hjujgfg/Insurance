1dedda09d4c28cf0966ee5609b52e214615178cd
Stafeev <dunlop300g@yandex.ru>
2014-02-02 20:14:35 +0400

    Action Buttons

--- gen/com/tteam/insurance/R.java
+++ gen/com/tteam/insurance/R.java
@@ -214,20 +214,28 @@ containing a value of this type.
         public static final int common_signin_btn_text_normal_light=0x7f020015;
         public static final int common_signin_btn_text_pressed_dark=0x7f020016;
         public static final int common_signin_btn_text_pressed_light=0x7f020017;
-        public static final int ic_launcher=0x7f020018;
-        public static final int ic_plusone_medium_off_client=0x7f020019;
-        public static final int ic_plusone_small_off_client=0x7f02001a;
-        public static final int ic_plusone_standard_off_client=0x7f02001b;
-        public static final int ic_plusone_tall_off_client=0x7f02001c;
+        public static final int ic_action_help=0x7f020018;
+        public static final int ic_action_paste=0x7f020019;
+        public static final int ic_action_refresh=0x7f02001a;
+        public static final int ic_action_select_all=0x7f02001b;
+        public static final int ic_launcher=0x7f02001c;
+        public static final int ic_plusone_medium_off_client=0x7f02001d;
+        public static final int ic_plusone_small_off_client=0x7f02001e;
+        public static final int ic_plusone_standard_off_client=0x7f02001f;
+        public static final int ic_plusone_tall_off_client=0x7f020020;
     }
     public static final class id {
         public static final int action_settings=0x7f050006;
+        public static final int badges_page=0x7f05000a;
         public static final int exit_app=0x7f050007;
+        public static final int help_page=0x7f05000b;
         public static final int hybrid=0x7f050004;
         public static final int imageView1=0x7f050005;
+        public static final int journeys_page=0x7f050009;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
         public static final int satellite=0x7f050002;
+        public static final int score_page=0x7f050008;
         public static final int terrain=0x7f050003;
     }
     public static final class integer {
@@ -265,6 +273,7 @@ containing a value of this type.
         [CHAR LIMIT=70] 
          */
         public static final int auth_client_using_bad_version_title=0x7f060014;
+        public static final int badges_page=0x7f060021;
         /**  Button in confirmation dialog to enable Google Play services.  Clicking it
         will direct user to application settings of Google Play services where they
         can enable it [CHAR LIMIT=40] 
@@ -338,6 +347,9 @@ containing a value of this type.
         public static final int common_signin_button_text_long=0x7f060013;
         public static final int exit=0x7f06001d;
         public static final int hello_world=0x7f06001c;
+        public static final int help_page=0x7f060022;
+        public static final int journeys_page=0x7f060020;
+        public static final int score_page=0x7f06001f;
         public static final int start_btn=0x7f06001e;
     }
     public static final class style {
